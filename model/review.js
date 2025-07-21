2eca56549152c3b164d9e4159847a7bc7c68af46
YoonWadiMyo <167498775+YoonWadiMyo@users.noreply.github.com>
2025-07-21 23:47:31 +0800

    Create review.js

---
+++ model/review.js
@@ -0,0 +1,12 @@
+class Review {
+    constructor() {
+        this.id = null;
+        this.sku = null;
+        this.rating = null;
+        this.title = null;
+        this.content = null;
+        this.author = null;
+        this.createdAt = null;
+    }
+}
+module.exports = Review;
